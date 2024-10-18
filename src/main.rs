6701ff4c4950323b1e5b0239a8a3132e66b169ad
KantLex <alexhuimagic@gmail.com>
2024-10-18 15:52:31 +0800

    Helper Function read_input

--- src/main.rs
+++ src/main.rs
@@ -80,3 +80,19 @@ fn add_contact(contacts: $mut Vec<Contact>) {
     println!("Contact added!");
 }
 
+fn read_input(prompt: &str) -> String {
+    println!(
+        "{}",
+        prompt
+    );
+    io::stdout().flush().expect("Failed to flush stdout");
+
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    input.trim().to_string()
+}
+
+
