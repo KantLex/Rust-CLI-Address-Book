bbb871880216b897750cbcb3e30029eb0cff4e13
KantLex <alexhuimagic@gmail.com>
2024-10-18 15:40:32 +0800

    Implement Main Function Structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)]
+struct Contact {
+    name: String,
+    phone: String,
+    email: Option<String>,
 }
+
+enum MenuChoice {
+    Add,
+    List,
+    Search,
+    Quit,
+}
+
+use std::io::{self, Write};
+
+fn main() {
+    let mut contacts: Vec<Contact> = Vec::new();
+
+    loop {
+        match menu() {
+            MenuChoice::Add => add_contact(&mut contacts),
+            MenuChoice::List => list_contacts(&contacts),
+            MenuChoice::Search => search_contacts(&contacts),
+            MenuChoice::Quit => {
+                println!("Goodbye!");
+                break;
+            }
+        }
+    }
+}
+
